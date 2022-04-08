a5309287bbd2a524e7adbf07b99b4b20297df1cb
Ngoni Munyaradzi <ngoni.munyaradzi@gmail.com>
2022-04-08 10:05:43 +0200

    Remove unused libraries

--- app/src/test/kotlin/GildedRoseTest.kt
+++ app/src/test/kotlin/GildedRoseTest.kt
@@ -10,6 +10,6 @@ class GildedRoseTest {
         val items = arrayOf(Item("foo", 0, 0))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+        assertEquals("foo", app.items[0].name)
     }
 }
