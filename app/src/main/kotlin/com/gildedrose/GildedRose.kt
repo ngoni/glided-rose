24afbfda8ce9ae5cce11eb191da6203679644ff7
Ngoni Munyaradzi <ngoni.munyaradzi@gmail.com>
2022-04-08 16:50:58 +0200

    Create function isBackstagePass()

--- app/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ app/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -5,7 +5,7 @@ class GildedRose(val items: Array<Item>) {
     fun updateQuality() {
         for (i in items.indices) {
             if (!isAgedBrie(i)
-                && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")
+                && !isBackstagePass(i)
             ) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
@@ -15,7 +15,7 @@ class GildedRose(val items: Array<Item>) {
             } else {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (isBackstagePass(i)) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1
@@ -34,7 +34,7 @@ class GildedRose(val items: Array<Item>) {
             }
             if (items[i].sellIn < 0) {
                 if (!isAgedBrie(i)) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (!isBackstagePass(i)) {
                         if (items[i].quality > 0) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality = items[i].quality - 1
@@ -51,6 +51,9 @@ class GildedRose(val items: Array<Item>) {
             }
         }
     }
+
+    private fun isBackstagePass(i: Int) =
+        items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")
 
     private fun isAgedBrie(i: Int) = items[i].name.equals("Aged Brie")
 }
