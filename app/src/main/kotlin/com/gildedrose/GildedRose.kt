318ee5fa26e5660d123705abbc0da874e3264ab8
Ngoni Munyaradzi <ngoni.munyaradzi@gmail.com>
2022-04-08 18:20:46 +0200

    Use generic function to check for Item type

--- app/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ app/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -13,20 +13,20 @@ class GildedRose(val items: Array<Item>) {
 
             decrementSellIn(item)
 
-            if (!isAgedBrie(item)
-                && !isBackstagePass(item)
+            if (!isItemType(item, ITEM_AGED_BRIE)
+                && !isItemType(item, ITEM_BACK_STAGE_PASS)
             ) {
                 decrementItemQuality(item)
             } else {
                 incrementItemQuality(item)
-                if (isBackstagePass(item)) {
+                if (isItemType(item, ITEM_BACK_STAGE_PASS)) {
                     incrementBackstagePassQuality(item)
                 }
             }
 
             if (isSellInLessThan(item, 0)) {
-                if (!isAgedBrie(item)) {
-                    if (!isBackstagePass(item)) {
+                if (!isItemType(item, ITEM_AGED_BRIE)) {
+                    if (!isItemType(item, ITEM_BACK_STAGE_PASS)) {
                         decrementItemQuality(item)
                     } else {
                         dropQualityToZero(item)
@@ -61,22 +61,17 @@ class GildedRose(val items: Array<Item>) {
 
     private fun decrementItemQuality(item: Item) {
         if (item.quality > 0) {
-            if (!isLegendary(item)) {
+            if (!isItemType(item, ITEM_LEGENDARY)) {
                 item.quality = item.quality - 1
             }
         }
     }
 
     private fun decrementSellIn(item: Item) {
-        if (!isLegendary(item)) {
+        if (!isItemType(item, ITEM_LEGENDARY)) {
             item.sellIn = item.sellIn - 1
         }
     }
 
-    private fun isLegendary(item: Item) = item.name.equals(ITEM_LEGENDARY)
-
-    private fun isBackstagePass(item: Item) =
-        item.name.equals(ITEM_BACK_STAGE_PASS)
-
-    private fun isAgedBrie(item: Item) = item.name.equals(ITEM_AGED_BRIE)
+    private fun isItemType(item: Item, name: String) = item.name == name
 }
